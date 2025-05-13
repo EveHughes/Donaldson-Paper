ffc8545b56ceeb38e9bd4592ec21ecf0e80bc4a5
Evelyn Hughes <evelyn.hughes@mail.utoronto.ca>
2025-05-13 11:16:47 -0400

    got graphs & tables to work, still need reformatting

--- scripts/05-exploratory_data_analysis.py
+++ scripts/05-exploratory_data_analysis.py
@@ -9,65 +9,12 @@
 
 import polars as pl
 import matplotlib.pyplot as plt
-import numpy as np
 
-t = np.arange(2019, 2025, 1)
-KEYS = ["Coronavirus", "Respiratory", "Total"]
-df = pl.read_csv("data/02-analysis_data/yearly_disease_count.csv")
-raw_data = df.to_dict()
-for key in KEYS:
-  plt.plot(t, raw_data[key], marker = 'o', label = key)
+raw_data = pl.read_csv("data/02-analysis_data/disease_count.csv")
+raw_data = raw_data.head(5)
+raw_data = raw_data.to_pandas()
+plt.table(raw_data)
 
-plt.title("Coronavirus, Respiratory & Total Outbreaks")
-plt.xlabel("Year")
-plt.ylabel("Outbreak Count")
-plt.grid()
-
-plt.legend()
 plt.show()
-
-
-#### Workspace setup ####
-# import numpy as np
-# import polars as pl
-# import matplotlib.pyplot as plt
-
-# #### Read data ####
-# composition_data = pl.read_csv("data/02-analysis_data/disease_count.csv")
-# dict1 = composition_data.to_dict(as_series = False)
-# analysis_data = pl.read_csv("data/02-analysis_data/yearly_disease_count.csv")
-
-
-# ### Model data ####
-# t = np.arange(2019, 2025, 1)
-# DISEASES = ["Influenza", "Coronavirus", "Syncytial Virus", "Metapneumovirus", "Rhinovirus", "Parainfluenza"]
-# SMALLER = ["Syncytial Virus", "Metapneumovirus", "Rhinovirus", "Parainfluenza"]
-# LARGER = {"Coronavirus", "Respiratory", "Total"}
-
-# print(dict1)
-# COLUMNS = ["Coronavirus", "Other", "Unknown"]
-# for key in COLUMNS:
-#   for i in range(6):
-#     dict1[key][i] = 100*dict1[key][i]/dict1["Total Agents"][i]
-
-# print(dict1)
-# corona = np.array(dict1["Coronavirus"])
-# other = np.array(dict1["Other"])
-# unknown = np.array(dict1["Unknown"])
-
-# ind = np.arange(6)    
-# width = 0.35       
-
-# p1 = plt.bar(ind, corona, width, color='#d62728', )
-# p2 = plt.bar(ind, other, width,  bottom=corona)
-# p3 = plt.bar(ind, unknown, width,  bottom=corona+other)
-
-# plt.ylabel('Percentage of Causative Agents')
-# plt.title('Composition of Respiratory Disease Outbreaks')
-# plt.xticks(ind, ('2019', '2020', '2021', '2022', '2023', '2024'))
-# plt.yticks(np.arange(0, 110, 10))
-# plt.legend((p1[0], p2[0], p3[0]), ('Coronavirus', 'Other', "Unknown"))
-
-# plt.show()
 
 #### Save model ####
